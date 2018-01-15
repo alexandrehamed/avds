d14caf77a586503d6f6c2dde7ee910cc480e76f1
Rayan Mahanna <rayan.mahanna@devinci.fr>
2018-01-15 19:21:34 +0100

    Section 1 (almost finished)

--- index.php
+++ index.php
@@ -1,20 +1,44 @@
-<?php get_header();?>
-
-
+<?php get_header(); ?>
 
 
 <!--Slide 2 Qui sommes nous ? -->
+<section id="section1">
+    <div class="container">
+        <div class="row">
+            <div class="col-8 mx-auto" id="section1h1h3">
+
+                <h1 class="center">Automobile Val de Seine</h1>
+                <br>
+                <h3>Achat et vente de véhicules d'occasion</h3>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="col-3 offset-md-2">
+                    <h4> Tous nos véhicules </h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
 
 
 <section id="about">
     <div class="container">
         <div class="row">
-            <div >
-            <div class="col-5 mx-auto" id="about_texte">
-                <h3 class="center"><strong>Qui sommes nous ?</strong></h3>
-                <br>
-                <p style="text-align: justify">Quapropter a natura mihi videtur potius quam ab indigentia orta amicitia, applicatione magis animi cum quodam sensu amandi quam cogitatione quantum illa res utilitatis esset habitura. Quod quidem quale sit, etiam in bestiis quibusdam animadverti potest, quae ex se natos ita amant ad quoddam tempus et ab eis ita amantur ut facile earum sensus appareat. Quod in homine multo est evidentius, primum ex ea caritate quae est inter natos et parentes, quae dirimi nisi detestabili scelere non potest; </p>
-            </div>
+            <div>
+                <div class="col-5 mx-auto" id="about_texte">
+                    <h3 class="center"><strong>Qui sommes nous ?</strong></h3>
+                    <br>
+                    <p style="text-align: justify">Quapropter a natura mihi videtur potius quam ab indigentia orta
+                        amicitia, applicatione magis animi cum quodam sensu amandi quam cogitatione quantum illa res
+                        utilitatis esset habitura. Quod quidem quale sit, etiam in bestiis quibusdam animadverti potest,
+                        quae ex se natos ita amant ad quoddam tempus et ab eis ita amantur ut facile earum sensus
+                        appareat. Quod in homine multo est evidentius, primum ex ea caritate quae est inter natos et
+                        parentes, quae dirimi nisi detestabili scelere non potest; </p>
+                </div>
             </div>
         </div>
 
@@ -24,17 +48,24 @@
             <div class="col-12">
                 <div id="container_icone">
                     <div class="row center">
-                        <div class="col-2 offset-md-2"><img src="<?php echo get_template_directory_uri();?>/images/icone1.png" class="img-fluid" alt=""><br><p>Sécurité</p></div>
-                        <div class="col-2 offset-md-1"><img src="<?php echo get_template_directory_uri();?>/images/icone2.png" class="img-fluid" alt=""><br><p>Professionnalisme</p></div>
-                        <div class="col-2 offset-md-1"><img src="<?php echo get_template_directory_uri();?>/images/icone3.png" class="img-fluid" alt=""><br><p>Qualité</p></div>
+                        <div class="col-2 offset-md-2"><img
+                                    src="<?php echo get_template_directory_uri(); ?>/images/icone1.png"
+                                    class="img-fluid" alt=""><br>
+                            <p>Sécurité</p></div>
+                        <div class="col-2 offset-md-1"><img
+                                    src="<?php echo get_template_directory_uri(); ?>/images/icone2.png"
+                                    class="img-fluid" alt=""><br>
+                            <p>Professionnalisme</p></div>
+                        <div class="col-2 offset-md-1"><img
+                                    src="<?php echo get_template_directory_uri(); ?>/images/icone3.png"
+                                    class="img-fluid" alt=""><br>
+                            <p>Qualité</p></div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
 </section>
 
 
-<?php get_footer();?>
+<?php get_footer(); ?>
