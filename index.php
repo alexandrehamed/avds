4ad4c0bcea18e884a1798b3cad9c7b2b9c28d8b8
Rayan Mahanna <rayan.mahanna@devinci.fr>
2018-01-18 03:00:50 +0100

    Last section + footer done

--- index.php
+++ index.php
@@ -125,11 +125,11 @@
 <section id="contactform">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2618.5767039179786!2d2.170982215592226!3d48.980581679299625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e660e01fc47415%3A0x4472d885dfa7bdc!2s1+Rue+Jean+Lefebvre%2C+95530+La+Frette-sur-Seine!5e0!3m2!1sfr!2sfr!4v1516207627602"
                         width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
-            <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 offset-md-1">
+            <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12 offset-lg-2" id="form1">
                 <h1>Nous contacter</h1>
                 <form>
                     <div class="form-group">
