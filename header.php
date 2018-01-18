6d5162bfe54c1ced4070f1530d63523d1e91b5d1
Rayan Mahanna <rayan.mahanna@devinci.fr>
2018-01-18 15:28:22 +0100

    m

--- header.php
+++ header.php
@@ -8,18 +8,39 @@
     <title>Automobile Val de Seine</title>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 <body>
 
-<nav id="navbar">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-2 center"> <h1>test</h1></div>
-            <div class="col-2 center"> <h1>test</h1></div>
-            <div class="col-2 center"> <h1>test</h1></div>
-            <div class="col-2 center"> <h1>test</h1></div>
-            <div class="col-2 center"> <h1>test</h1></div>
-            <div class="col-2 center"> <h1>test</h1></div>
+<nav id="navbar_custom">
+<div class="container-fluid">
+    <div class="row full-nav">
+        <div class="col-1 center"><a href="#"><p>Ici logo</p></a></div>
+        <div class="col-2 offset-md-1 center"><a href="#"><p>L'entreprise</p></a></div>
+        <div class="col-2 center"><a href="#"><p>Nos partenaires</p></a></div>
+        <div class="col-2 center"><a href="#"><p>Nos véhicules</p></a> </div>
+        <div class="col-2 center"><a href="#"><p>Nous contacter</p></a></div>
+        <div class="col-1 offset-md-1 center"><a href="#"><i class="fa fa-phone fa-2x" aria-hidden="true" style="color: white"></i></a> </div>
+    </div>
+    <div class="row responsive-nav ">
+        <div class="pos-f-t">
+            <div class="collapse" id="navbarToggleExternalContent">
+                <div class="p-4 center">
+                    <li><a href="#"><p>L'entreprise</p></a></li>
+                    <li><a href="#"><p>Nos partenaires</p></a></li>
+                    <li<a href="#"><p>Nos véhicules</p></a>></li>
+                    <li><a href="#"><p>Nous contacter</p></a></li>
+                    <li><a href="#"><i class="fa fa-phone fa-2x" aria-hidden="true" style="color: white"></i></a></li>
+
+                </div>
+            </div>
+                <nav class="navbar ">
+                    <button class="navbar-toggler ml-auto " type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+                    </button>
+                </nav>
+
         </div>
     </div>
 </nav>
